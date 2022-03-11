6c3bc6502407221d36e1971c0965c5d38de54af1
William Pilger <40075505+williampilger@users.noreply.github.com>
2022-03-11 11:08:37 -0300

    Create class.php

---
+++ BaseFiles/php/class.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Authenty AE - Bom Princípio-RS  |  github.com/authentyAE
+ * by: will.i.am                   |  github.com/williampilger
+ *
+ * 2022.02.23 - Bom Princípio - RS
+ * ♪ - / -
+ *  
+ * Classe responsável por ... .
+ * 
+ */
+require_once __DIR__."/../config.php";
+
+class Classe
+{
+
+    public function __construct()
+    {
+        
+    }
+
+    // #####################################################################################################################
+    // # Métodos PÚBLICOS - Métodos PÚBLICOS - Métodos PÚBLICOS - Métodos PÚBLICOS - Métodos PÚBLICOS - Métodos PÚBLICOS - #
+    // #####################################################################################################################
+
+    // #####################################################################################################################
+    // # Métodos PÚBLICOS ESTÁTICOS - Métodos PÚBLICOS ESTÁTICOS - Métodos PÚBLICOS ESTÁTICOS - Métodos PÚBLICOS ESTÁTICOS #
+    // #####################################################################################################################
+
+    // #####################################################################################################################
+    // # Métodos PRIVADOS - Métodos PRIVADOS - Métodos PRIVADOS - Métodos PRIVADOS - Métodos PRIVADOS - Métodos PRIVADOS - #
+    // #####################################################################################################################
+
+    // #####################################################################################################################
+    // # Métodos PRIVADOS ESTÁTICOS - Métodos PRIVADOS ESTÁTICOS - Métodos PRIVADOS ESTÁTICOS - Métodos PRIVADOS ESTÁTICOS #
+    // #####################################################################################################################
+
+}
+
+?>
