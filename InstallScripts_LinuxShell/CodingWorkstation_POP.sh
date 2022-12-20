b23ef211c25ce110e9185fba6190d88238986d41
williampilger <pilger.will@gmail.com>
2022-12-20 09:47:45 -0300

    upld

--- InstallScripts_LinuxShell/CodingWorkstation_POP.sh
+++ InstallScripts_LinuxShell/CodingWorkstation_POP.sh
@@ -1,44 +1,128 @@
 #!/bin/bash
-## VERSÃO DO SISTEMA: 22.04 LTS
+## VERSÃO DO SISTEMA: POP! OS - 22.04 LTS
+
+LOG(){
+	CONTENT=$1
+	echo $(date) - $CONTENT >> LOG.txt
+}
+
+apt_install(){
+	SOFTWARE=$1
+	if ! dpkg -l | grep -q $SOFTWARE; then
+		sudo apt-get install -y $SOFTWARE
+		if [ "$?" == 0 ]; then
+			LOG "	2212200855 - Successfully install SYSTEM $SOFTWARE."
+		else
+			LOG "	2212200853 - Impossible install SYSTEM $SOFTWARE."
+		fi
+	else
+		LOG "	2212200924 - Allready installed SYSTEM $SOFTWARE."
+	fi
+}
+
+flatpack_install(){
+	SOFTWARE=$1
+	flatpak install flathub -y $SOFTWARE
+	if [ "$?" == 0 ]; then
+		LOG "	2212200905 - Successfully install FLATPACK $SOFTWARE."
+	else
+		LOG "	2212200906 - Impossible install FLATPACK $SOFTWARE."
+	fi
+}
+
+snap_install(){
+	SOFTWARE=$1
+	sudo snap install $SOFTWARE
+	if [ "$?" == 0 ]; then
+		LOG "	2212200907 - Successfully install SNAP $SOFTWARE."
+	else
+		LOG "	2212200908 - Impossible install SNAP $SOFTWARE."
+	fi
+}
+
+LOG '2212200909 - Start Script. Updating...'
 
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# Geral-Basico - DIVERSOS
-sudo apt-get install -y net-tools gparted nmap guvcview samba xclip gdebi remmina
-sudo apt-get install -y p7zip p7zip-full p7zip-rar
-sudo apt-get install -y gnome-boxes
-sudo apt-get install -y snapd
-sudo snap install homeserver
+LOG '2212200910 - Start System APPs instalation:'
 
-# Geral-Basico - CODING
-sudo apt-get install -y python3 python3-pip
-sudo apt-get install -y git curl filezilla
-flatpak install flathub -y com.getpostman.Postman
-flatpak install flathub -y com.visualstudio.code
-flatpak install flathub -y org.kde.kcolorchooser
-flatpak install flathub -y md.obsidian.Obsidian
+APT_PROGRAMS=(
+	# Geral
+	net-tools
+	gparted
+	nmap
+	guvcview
+	samba
+	xclip
+	gdebi
+	remmina
+	p7zip
+	p7zip-full
+	p7zip-rar
+	gnome-boxes
+	snapd
+	wine
+	winetricks
+	# Codding
+	python3
+	python3-pip
+	git
+	curl
+	filezilla
+	# OBS Studio
+	obs-studio
+	linux-headers-$(uname -r)
+	v4l2loopback-dkms
+	# Publicidade-Imagens-Edição
+	gimp
+	inkscape
+	imagemagick
+	vlc
+)
+for nome_do_programa in ${APT_PROGRAMS[@]}; do
+	apt_install $nome_do_programa
+done
 
-# Publicidade-Imagens-Edição
-sudo apt-get install -y obs-studio linux-headers-$(uname -r) v4l2loopback-dkms
-sudo apt-get install -y gimp inkscape imagemagick vlc
-###sudo snap install flameshot
-flatpak install flathub -y flameshot
-flatpak install flathub -y org.onlyoffice.desktopeditors
-flatpak install flathub -y com.github.xournalpp.xournalpp
-flatpak install flathub -y com.uploadedlobster.peek
-flatpak install flathub -y com.github.maoschanz.drawing
-flatpak install flathub -y net.xmind.XMind
-flatpak install flathub com.obsproject.Studio
 
-# Comunicação-Social
-###snap install telegram-desktop
-flatpak install flathub -y io.github.mimbrero.WhatsAppDesktop
-flatpak install flathub -y telegram-desktop
+LOG '2212200911 - Start Flatpack APPs instalation:'
 
-# Spotify
-flatpak install flathub -y com.spotify.Client
-###sudo snap install spotify
+FLATPACK_PROGRAMS=(
+	# Codding
+	com.getpostman.Postman
+#	com.visualstudio.code
+	org.kde.kcolorchooser
+	md.obsidian.Obsidian
+	# Publicidade-Imagens-Edição
+	flameshot
+	org.onlyoffice.desktopeditors
+	com.github.xournalpp.xournalpp
+	com.uploadedlobster.peek
+	com.github.maoschanz.drawing
+	# Comunicação-Social
+	telegram-desktop
+	io.github.mimbrero.WhatsAppDesktop
+	# Geral
+	com.usebottles.bottles
+	com.spotify.Client
+)
+for nome_do_programa in ${FLATPACK_PROGRAMS[@]}; do
+	flatpack_install $nome_do_programa
+done
+
+LOG '2212200912 - Start Snap APPs instalation:'
+
+SNAP_PROGRAMS=(
+	# Geral
+	homeserver
+)
+for nome_do_programa in ${SNAP_PROGRAMS[@]}; do
+	snap_install $nome_do_programa
+done
+
+
+LOG '2212200913 - Start Custom instalations:'
+
 
 # Google Chrome
 cd /home/$USER/Downloads
@@ -58,6 +142,10 @@ curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get update
 sudo apt --fix-broken install -y -f nodejs
 sudo npm install --global yarn
+
+
+LOG '2212200931 - Start Other configurations:'
+
 
 #WakeOnLan
 setup_WakeOnLan(){
@@ -79,49 +167,16 @@ WantedBy=basic.target
 }
 setup_WakeOnLan
 
-#RustDesk
-setup_rustDesk(){
-	URL="https://github.com/rustdesk/rustdesk/releases/download/1.1.9/RustDesk-1.1.10-x86_64_1804.AppImage"
-	mkdir /AppImages
-	wget $URL -o /AppImages/RustDesk.AppImage
-	chmod +x /AppImages/RustDesk.AppImage
-	echo """
-[Unit]
-Description=Inicio automático do RustDesk
-After=network.target
+# SSH Github
+ssh-keygen -t ed25519 -C "GitHub - WilliamPilger - MICRO-02 - Escritório"
+xclip -sel clip < ~/.ssh/id_ed25519.pub
+LOG "NEW SSH KEYPAIR GENERATED: $(cat ~/.ssh/id_ed25519.pub)"
 
-[Service]
-Type=simple
-LimitNOFILE=1024
 
-Restart=on-failure
-RestartSec=10
-startLimitIntervalSec=60
-Environment=DISPLAY=:0
-ExecStart=/AppImages/RustDesk.AppImage
+LOG '2212200938 - Post-install finished.'
 
-PermissionsStartOnly=true
-StandardOutput=syslog
-StandardError=syslog
-
-[Install]
-WantedBy=multi-user.target
-	""" > /etc/systemd/system/RustDesk.service
-	systemctl daemon-reload
-	systemctl enable wol.service
-}
-setup_rustDesk
-
-#Wine
-flatpak install flathub -y com.usebottles.bottles
-apt-get install -y wine winetricks
-winetricks dotnet48
 
 # Resta instalar manualmente
 # VS Code (DEB. pois flatpack não funciona terminal)
 # insync
 # Virtual Box
-
-# SSH Github
-ssh-keygen -t ed25519 -C "GitHub - WilliamPilger - MICRO-02 - Escritório"
-xclip -sel clip < ~/.ssh/id_ed25519.pub
