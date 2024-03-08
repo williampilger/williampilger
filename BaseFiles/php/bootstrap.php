f18b4bac4e415ddda668fefd055357488c449922
William Pilger <40075505+williampilger@users.noreply.github.com>
2024-03-08 11:23:36 -0300

    Update bootstrap.php

--- BaseFiles/php/bootstrap.php
+++ BaseFiles/php/bootstrap.php
@@ -1,23 +1,27 @@
 <?php
 
 // Função para carregar as variáveis do .env
-function loadEnv(string $path='.', bool $advSecurity=true ) {
-  $env = [];
-  $file = fopen("$path/.env", 'r');
-  while (($line = fgets($file)) !== false) {
-    $line = trim($line);
-    if (empty($line) || $line[0] === '#') {
-      continue;
-    }
-    list($key, $value) = explode('=', $line, 2);
-    if( $$advSecurity )
+function loadEnv(string $path='.', bool $advSecurity=true )
+{
+    $env = [];
+    $file = fopen("$path/.env", 'r');
+    while (($line = fgets($file)) !== false)
     {
-      $value = trim($value, '"'); // Remove aspas duplas
-      $value = stripcslashes($value); // Remove backslashes
+        $line = trim($line);
+        if (empty($line) || $line[0] === '#')
+        {
+            continue;
+        }
+        list($key, $value) = explode('=', $line, 2);
+        if( $$advSecurity )
+        {
+            $value = trim($value, '"'); // Remove aspas duplas
+            $value = stripcslashes($value); // Remove backslashes
+        }
+        $env[$key] = $value;
     }
-    $env[$key] = $value;
-  }
-  fclose($file);
-  $GLOBALS['_ENV'] = $env;
+    fclose($file);
+    $GLOBALS['_ENV'] = $env;
 }
 loadEnv();
+?>
